11c978e6c1bdb29ef5a99a4a37b13e61af18c727
alex <tsirasalexis@yahoo.gr>
2019-04-11 11:49:33 +0300

    Add open weather map appid to fetch call

--- lib/weather/fetch_data.ex
+++ lib/weather/fetch_data.ex
@@ -3,6 +3,10 @@ defmodule Weather.FetchData do
 
   def fetch_by_city(city) do
     Logger.info("Fetching weather data for #{city}")
+
+    city_url(city)
+    |> HTTPoison.get()
+    |> handle_response
   end
 
   def fetch_by_latlong(lat, long) do
@@ -12,4 +16,12 @@ defmodule Weather.FetchData do
   def fetch_by_zipcode(zip_code) do
     Logger.info("Fetching weather data for zipcode: #{zip_code}")
   end
+
+  defp city_url(city) do
+    appid = System.get_env("OPEN_WEATHER_APPID")
+    "api.openweathermap.org/data/2.5/forecast?q=#{city}&appid=#{appid}"
+  end
+
+  defp handle_response(%{status: 200, body: body}), do: {:ok, body}
+  defp handle_response(%{status: _, body: body}), do: {:error, body}
 end
